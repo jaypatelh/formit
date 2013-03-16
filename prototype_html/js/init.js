44d90b2bfa8f7d16fc36f72faf9e9fc285cd244b
Kevin Ho <keho98@gmail.com>
2013-03-16 15:47:23 -0700

    Trying out 2 finger swipe

--- prototype_html/js/init.js
+++ prototype_html/js/init.js
@@ -17,25 +17,34 @@ $(document).ready(function(){
 		$('#next').prev().attr('class','thumb selected_thumb');
 		$('#formation_number').html(dance.formations.length + 1);
 	});
-	$('svg').hammer().on('swipeleft', function(){
-		// go to next formation if it exists, else create new formation
-		var bool = dance.nextFormation();
-		dance.deselectAll();
-		// ADD THUMBNAIL TO TIMELINE - only in case nextFormation calls newFormation
-		if(!bool && dance.atEnd()) {
-			// add thumbnail to timeline
-			$('#next').before("<div class='thumb'><svg></svg></div>");
-			// make thumbnail selected
-			var children = $('.thumbnail_container').children('.thumb');
-			children.attr('class','thumb');
-			$('#next').prev().attr('class','thumb selected_thumb');
+	$('.stage').hammer().on('pinch', function(e){
+		console.log("Pinch Detected...")
+		console.log(e)
+	})
+	$('.stage').hammer().on('swipeleft', function(e){
+		if(e.gesture.touches.length > 1){
+			// go to next formation if it exists, else create new formation
+			var bool = dance.nextFormation();
+			dance.deselectAll();
+			// ADD THUMBNAIL TO TIMELINE - only in case nextFormation calls newFormation
+			if(!bool && dance.atEnd()) {
+				// add thumbnail to timeline
+				$('#next').before("<div class='thumb'><svg></svg></div>");
+				// make thumbnail selected
+				var children = $('.thumbnail_container').children('.thumb');
+				children.attr('class','thumb');
+				$('#next').prev().attr('class','thumb selected_thumb');
+			}
+			$('#formation_number').html(dance.f_id + 1);
 		}
-		$('#formation_number').html(dance.f_id + 1);
 	});
-	$('svg').hammer().on('swiperight', function(){
-		dance.previousFormation();
-		dance.deselectAll();
-		$('#formation_number').html(dance.f_id + 1)
+
+	$('.stage').hammer().on('swiperight', function(e){
+		if(e.gesture.touches.length > 1){
+			dance.previousFormation();
+			dance.deselectAll();
+			$('#formation_number').html(dance.f_id + 1);
+		}
 	});
 	$('.timeline').hammer().on('tap', '.thumb', function(){
 		var curr_thumb = $(this);
