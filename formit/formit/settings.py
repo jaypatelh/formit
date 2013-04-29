eb70cdafe089d3d1440688b51d2b7caeced0007d
Jay Patel <jayhp9@stanford.edu>
2013-04-29 12:24:09 -0700

    created new db.sqlite3 and put dj_database_url in settings properly

--- formit/formit/settings.py
+++ formit/formit/settings.py
@@ -14,17 +14,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='sqlite:////Users/jaypatel/Stanford/Spring2012-2013/formit/formit/formitapp/db.sqlite3')
 }
-
-DATABASES['default'] =  dj_database_url.config()
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
